80509abe0cd283d511aa19758ce721cfed70dc5f
= <=>
2021-12-08 16:41:24 +0800

    handle cross origin

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors);
 
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
