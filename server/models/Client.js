c42d55b26be1c640043b5079f339e57fc7178469
= <=>
2021-12-08 12:07:35 +0800

    minor fix

--- server/models/Client.js
+++ server/models/Client.js
@@ -7,30 +7,12 @@ module.exports = (sequelize, DataTypes) => {
     birthday: DataTypes.DATEONLY,
     isDead: DataTypes.BOOLEAN,
     householdadmin: DataTypes.BOOLEAN,
-    incomeStatus: DataTypes.ENUM('low', 'mid-low', 'mid-low-elderly'),
-    disabledStatus: DataTypes.ENUM('light', 'mid', 'heavy'),
-
-    group: DataTypes.ENUM(
-      '阿美',
-      '泰雅',
-      '布農',
-      '卡那卡那富',
-      '噶瑪蘭',
-      '排灣',
-      '卑南',
-      '魯凱',
-      '拉阿魯哇',
-      '賽夏',
-      '撒奇萊雅',
-      '賽德克',
-      '太魯閣',
-      '邵',
-      '鄒',
-      '雅美',
-    ),
+    // TODO: extract enum list to a config file
+    incomeStatus: DataTypes.ENUM('低收入戶', '中低收入戶', '中低收老人', '經濟困難', '邊緣戶'),
+    disabledStatus: DataTypes.ENUM('輕度', '中度', '重度', '極重度'),
+    group: DataTypes.ENUM('阿美', '泰雅', '布農', '卡那卡那富', '噶瑪蘭', '排灣', '卑南', '魯凱', '拉阿魯哇', '賽夏', '撒奇萊雅', '賽德克', '太魯閣', '邵', '鄒', '雅美'),
     tribe: DataTypes.STRING,
     plainMountain: DataTypes.ENUM('平原', '山原'),
-
     canCall: DataTypes.BOOLEAN,
     phone: DataTypes.STRING,
     phoneNote: DataTypes.STRING,
@@ -40,7 +22,6 @@ module.exports = (sequelize, DataTypes) => {
     mobileNote: DataTypes.STRING,
     mobileAlt: DataTypes.STRING,
     mobileAltNote: DataTypes.STRING,
-
     canMail: DataTypes.BOOLEAN,
     city: DataTypes.STRING,
     cityAlt: DataTypes.STRING,
@@ -55,11 +36,9 @@ module.exports = (sequelize, DataTypes) => {
     note: DataTypes.TEXT,
   });
 
-  const ClinetClients = sequelize.define(
+  sequelize.define(
     'ClientClients',
-    {
-      relationship: DataTypes.TEXT,
-    },
+    { relationship: DataTypes.TEXT },
     { timestamps: false },
   );
 
